9bfcf2770a506ae01afd5009e2c1f17f1bef56dd
Puskar Uprety <upretypuskar@gmail.com>
2022-12-20 11:59:27 -0500

    The feauture of the car have been added successful

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         CarFeatures features = new CarFeatures();
         System.out.println("Launch the car");
+        features.color="red";
+        features.doors="5";
+        features.engine=3233;
+        features.model="a5";
     }
 }
